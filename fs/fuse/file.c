49c3e84652f97fbdb636d1188ac7f8724b108ba2
Faris <90097027+rsuntk@users.noreply.github.com>
2025-10-23 10:29:23 +0000

    fuse: Import minimal Samsung changes
    Signed-off-by: Faris <90097027+rsuntk@users.noreply.github.com>
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -19,6 +19,7 @@
 #include <linux/swap.h>
 #include <linux/falloc.h>
 #include <linux/uio.h>
+#include <linux/sched/mm.h>
 #include <linux/fs.h>
 
 static struct page **fuse_pages_alloc(unsigned int npages, gfp_t flags,
@@ -482,7 +483,7 @@ static void fuse_wait_on_page_writeback(struct inode *inode, pgoff_t index)
 {
 	struct fuse_inode *fi = get_fuse_inode(inode);
 
-	wait_event(fi->page_waitq, !fuse_page_is_writeback(inode, index));
+	fuse_wait_event(fi->page_waitq, !fuse_page_is_writeback(inode, index));
 }
 
 /*
@@ -2022,6 +2023,7 @@ int fuse_write_inode(struct inode *inode, struct writeback_control *wbc)
 	struct fuse_inode *fi = get_fuse_inode(inode);
 	struct fuse_file *ff;
 	int err;
+	unsigned int nofs_flag;
 
 	/**
 	 * TODO - fully understand why this is necessary
@@ -2036,22 +2038,13 @@ int fuse_write_inode(struct inode *inode, struct writeback_control *wbc)
 	if (!S_ISREG(inode->i_mode))
 		return 0;
 
-	/*
-	 * Inode is always written before the last reference is dropped and
-	 * hence this should not be reached from reclaim.
-	 *
-	 * Writing back the inode from reclaim can deadlock if the request
-	 * processing itself needs an allocation.  Allocations triggering
-	 * reclaim while serving a request can't be prevented, because it can
-	 * involve any number of unrelated userspace processes.
-	 */
-	WARN_ON(wbc->for_reclaim);
-
+	nofs_flag = memalloc_nofs_save();
 	ff = __fuse_write_file_get(fc, fi);
 	err = fuse_flush_times(inode, ff);
 	if (ff)
 		fuse_file_put(inode, ff, false, false);
 
+	memalloc_nofs_restore(nofs_flag);
 	return err;
 }
 
